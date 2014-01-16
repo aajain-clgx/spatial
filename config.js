56c50a598e5062fd63ea395509945f9788c502b3
adamvinueza <adamvinueza@gmail.com>
2014-01-16 15:04:27 -0700

    Added nodemailer. New require path conventions.

--- config.js
+++ config.js
@@ -3,24 +3,19 @@
 // Configuration file for spatial
 
 var os = require('os');
-var path = require('path');
-var config = {}
+var config = {};
 
-config.root = path.dirname(require.main.filename); 
 config.appName = 'spatial';
 config.machineName = os.hostname();
 config.common = {
-  utilsPath : path.join(config.root,'libs/common/utils'),
   logger : {
-    path : path.join(config.root, 'libs/common/logger'),
     logFilePath : 'f:\\logs\\spatial.txt',
     fileLevel : 'ERROR',
-    emailLevel : 'ERROR'
+    emailLevel : 'ERROR',
+    smtpServer : 'smtp.corelogic.com',
+    emailSender : 'logger@corelogic.com',
+    emailRecipient : 'avinueza@corelogic.com'
   }
-};
-config.external = {
-  dateFormatPath : path.join(config.root,'libs/external/dateformat'),
-  enumPath : path.join(config.root, 'libs/external/enum')
 };
 
 module.exports = config;
