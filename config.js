e845e11649d1a2d692a33eb3253ac2ca7df398c3
unknown <avinueza@BLD01WW72410TP6.infosolco.net>
2014-01-30 17:43:19 +0000

    Logger now takes config object in constructor.

--- config.js
+++ config.js
@@ -5,19 +5,15 @@
 'use strict';
 
 var os = require('os');
-var config = {};
-
-config.appName = 'spatial';
-config.machineName = os.hostname();
-config.common = {
-  logger : {
-    logFilePath : 'f:\\logs\\spatial.txt',
-    fileLevel : 'NOTICE',
-    emailLevel : 'ERROR',
-    smtpServer : 'smtp.corelogic.com',
-    emailSender : 'logger@corelogic.com',
-    emailRecipient : 'avinueza@corelogic.com'
-  }
+var config = {
+  appName : 'spatial',
+  machineName : os.hostname(),
+  logFilePath : 'f:\\logs\\spatial.txt',
+  fileLevel : 'NOTICE',
+  emailLevel : 'ERROR',
+  smtpServer : 'smtp.corelogic.com',
+  emailSender : 'logger@corelogic.com',
+  emailRecipient : 'avinueza@corelogic.com'
 };
 
 module.exports = config;
