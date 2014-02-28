d1b34efea81ac6249066da50faed5e4055300a03
safou <useraj@server.fake>
2014-02-28 14:18:11 -0700

    Added new clientlib

--- client/client.js
+++ client/client.js
@@ -6,32 +6,32 @@
 
 var restify = require('../spatial/lib/external/restify');
 var config = require('../config.js');
-var authenticator = require('../spatial/lib/common/authenticator.js');
-var authenticator = authenticator(config);
-var urllib = require('url');
+var clientlib = require('../spatial/lib/common/clientlib.js'); 
 
+// CREATE CLIENT
 
-var url = 'http://localhost:' + config.serverPort;
-
+var serverBaseURL = 'http://localhost:' + config.serverPort;
+var privatekey = '4qgCyF0qQrdyj34/g+C7dGfjJ7A=';
 var client = restify.createJsonClient({
-  url: url
+  url: serverBaseURL
 });
 
-var restCall = function() {
-  var timestamp =  Math.round(Date.now()/1000);
-  var tmpurl = url + '/v1/permissions/425032B8C0FB434992F69370FED9D071?timestamp=' + timestamp;
-  var signature = authenticator.getSignatureForRequest(tmpurl, '4qgCyF0qQrdyj34/g+C7dGfjJ7A=');
-  tmpurl = tmpurl + '&signature=' + signature;
-  var urlPath = urllib.parse(tmpurl).path;
+// INIT CLIENTLIB
 
-  client.get(urlPath, function(err, req, res, obj) {
+var clientlib = clientlib(privatekey, serverBaseURL);
+
+var getServer = function(urlPath) {
+  var fullURLObj = clientlib.getPathWithSignature(urlPath);
+  client.get(fullURLObj.path, function(err, req, res, obj) {
     if (err) {
       console.log(err);
+    }else{ 
+      console.log(JSON.stringify(obj, null, 2));
     }
-    console.log(JSON.stringify(obj, null, 2));
   });
 };
 
-restCall();
-restCall();
-restCall();
+// SHOW OUTPUT
+
+getServer('/v1/permissions/425032B8C0FB434992F69370FED9D071');
+getServer('/v1/info/425032B8C0FB434992F69370FED9D071');
