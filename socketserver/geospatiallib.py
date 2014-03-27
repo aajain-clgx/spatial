3a0007637b52176d9250760373b3d9421192d419
safou <useraj@server.fake>
2014-03-27 16:46:55 -0600

    fixed typo

--- socketserver/geospatiallib.py
+++ socketserver/geospatiallib.py
@@ -69,8 +69,8 @@ def create_json_result_with_status(
     result = []
     status = StatusCode.OK
     message = ""
+    socketid = -1
 
-    print "Return code: " + str(rc)
     if rc == pxcommon.PXP_SUCCESS:
         o_nrows = 0 if out_tbl is None else out_tbl.nrows()
         if o_nrows == 0:
@@ -79,22 +79,30 @@ def create_json_result_with_status(
         else:
             if max_results > o_nrows or max_results == -1:
                 max_results = o_nrows
-            # Dictionary comprehension inside list comprehension
 
-            print("max_results"+str(max_results))
+            # Dictionary comprehension inside list comprehension
             result = [
                 {sanitize_colname(out_tbl.col_names[i]):
                     str(out_tbl.rows[j][i]).encode("unicode_escape")
                     for i in xrange(out_tbl.ncols())}
                 for j in xrange(max_results)
             ]
+
+            socketid = result[0]['INPUT.Id']
+            for d in result:
+                del d['INPUT.Id']
     elif err_tbl is None or err_tbl.nrows == 0:
         status = StatusCode.SERVER_ERROR
         message = "Error table is empty, despite apparent error"
     else:
-        error_row = err_tbl.rows[0]
-        error_code = str(error_row[0])
-        error_message = str(error_row[1])
+        errors = [
+            {err_tbl.col_names[i]: str(err_tbl.rows[j][i]).encode("unicode_escape")
+                for i in xrange(err_tbl.ncols())}
+            for j in xrange(err_tbl.nrows())
+        ]
+        socketid = errors[0]['INPUT.Id']
+        error_code = errors[0]['error_code']
+        error_message = errors[0]['error_message']
         status = StatusCode.SERVER_ERROR
         if rc == pxcommon.PXP_INVALID_ARGUMENT:
             status = StatusCode.INVALID_REQUEST
@@ -106,6 +114,7 @@ def create_json_result_with_status(
     return_obj["result"] = result
     return_obj["status"] = status
     return_obj["message"] = message
+    return_obj["socketid"] = socketid
 
     return json.dumps(return_obj, sort_keys=True)
 
@@ -113,8 +122,9 @@ if __name__ == "__main__":
     # Test Code
     DEFAULT_LICENSE_FILE = r'pxpoint.lic'
     LICENSE_KEY = 123456789
-    DATASET_ROOT = r'/media/sf_pxse-data/geocode/PxPoint_2013_12'
+    DATASET_ROOT = r'//mnt/data/pxse-data/geocode/PxPoint_2013_12'
     from pxpoint import pxpointsc
+    import pprint
     (geocoder_handle, rc, msg) = pxpointsc.geocoder_init(
         DATASET_ROOT,
         ['NavteqStreet', 'Parcel', 'USPS'],
@@ -122,25 +132,23 @@ if __name__ == "__main__":
         LICENSE_KEY
     )
     input_table = create_geocode_input_table(
-        0, '3239 Redstone Road', 'Boulder CO'
+        4, '3239 Redstone Road', 'Boulder CO'
     )
 
-    out_tbl,
-    err_tbl,
-    rc,
-    msg = pxpointsc.geocoder_geocode(
+    out_tbl, err_tbl, rc, msg = pxpointsc.geocoder_geocode(
         geocoder_handle,
         input_table,
         GeoSpatialDefaults.GEOCODING_OUTPUT_COLS,
         GeoSpatialDefaults.ERROR_TABLE_COLS,
         GeoSpatialDefaults.BESTMATCH_FINDER_OPTIONS
     )
-    print err_tbl
-    print rc
+    pprint.pprint(out_tbl)
+    pprint.pprint(err_tbl)
+
     result = create_json_result_with_status(
         out_tbl, err_tbl, rc
     )
     result_dict = json.loads(result)
     print result_dict["status"]
-    import pprint
+
     pprint.pprint(result_dict)
